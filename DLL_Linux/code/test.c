f3012a22fe243c6d653452eeff933582ef0c6c57
l28210 <1771027219@qq.com>
2023-09-06 00:22:32 +0800

    first commit

---
+++ DLL_Linux/code/test.c
@@ -0,0 +1,7 @@
+#include"test.h"
+
+void print_hello(void)
+{
+	printf("hello world\n");
+}
+
---
+++ DLL_Linux/code/test.h
@@ -0,0 +1,8 @@
+#ifndef _TEST_H
+#define _TEST_H
+
+#include<stdio.h>
+
+void print_hello(void);
+
+#endif
