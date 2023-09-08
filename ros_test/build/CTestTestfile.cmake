c5da71cccb0b3a07bda8c28cd309968a860715ae
l28210 <1771027219@qq.com>
2023-09-08 11:02:21 +0800

    first commit

---
+++ ros_test/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/l/test_self/ros_test/src
+# Build directory: /home/l/test_self/ros_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ ros_test/build_isolated/cmd_vel_test/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/l/test_self/ros_test/devel_isolated/cmd_vel_test:$CMAKE_PREFIX_PATH"
+export PWD='/home/l/test_self/ros_test/build_isolated/cmd_vel_test'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/l/test_self/ros_test/devel_isolated/cmd_vel_test/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/l/test_self/ros_test/src/cmd_vel_test:$ROS_PACKAGE_PATH"
